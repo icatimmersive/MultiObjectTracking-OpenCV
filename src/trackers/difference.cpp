5c542b52a21c15172f9d64ec09d9ccfd316539bf
Anirudh Bagde <anidev.aelico@gmail.com>
2015-11-06 15:37:16 -0500

    Improved difference tracking maybe

--- src/trackers/difference.cpp
+++ src/trackers/difference.cpp
@@ -12,7 +12,7 @@ void smoothMask(cv::UMat& maskImage) {
     // Median filter for smoothing
     cv::medianBlur(maskImage, maskImage, 3); // MATLAB code uses medfilt2, which uses 3x3 aperture
     // Morphological operations to remove noise and fill in holes
-    cv::Mat kernel = cv::getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE, cv::Size(8, 8));
+    cv::Mat kernel = cv::getStructuringElement(cv::MorphShapes::MORPH_ELLIPSE, cv::Size(4, 4));
     cv::morphologyEx(maskImage, maskImage, cv::MorphTypes::MORPH_CLOSE, kernel);
 }
 
@@ -26,7 +26,7 @@ DifferenceTracker::DifferenceTracker() : skipped(0) {
     // Initialize difference engine and blob detector
     // Parameters taken from original MATLAB code
 //     diffEngine = cv::bgsegm::createBackgroundSubtractorMOG(100, 2, 0.01); // requires contrib module "bgsegm"
-    diffEngine = cv::createBackgroundSubtractorKNN(1000, 400.0, false);
+    diffEngine = cv::createBackgroundSubtractorKNN(1000, 800.0, true);
 //     diffEngine = cv::createBackgroundSubtractorMOG2(100, 400.0, true);
 }
 
@@ -35,7 +35,7 @@ DifferenceTracker::~DifferenceTracker() {
 
 void DifferenceTracker::processFrame(cv::UMat& frame) {
     // third parameter = rate of background update (0.0 is no update)
-    diffEngine->apply(frame, maskImage, 0.002);
+    diffEngine->apply(frame, maskImage, 0.001);
     // Apply operations to improve mask image
     smoothMask(maskImage);
     // Now return if this frame should be skipped
