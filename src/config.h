1b9bc89cfa5f13d714b1f2f03d14ab3b8e49af72
Anirudh Bagde <anidev.aelico@gmail.com>
2015-11-11 01:50:28 -0500

    Initial commit of Config class

---
+++ src/config.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <opencv2/core.hpp>
+
+struct CameraClass {
+    std::string name;
+    std::string username;
+    std::string password;
+    std::string path;
+};
+
+struct CameraInfo {
+    int id;
+    std::string name;
+    std::string description;
+    std::string className;
+    std::string ip;
+    cv::Rect crop;
+};
+
+class Config {
+public:
+    static Config& get();
+
+    ~Config();
+
+    std::string getServerURL();
+    int getServerPort();
+
+    bool isCameraDefined(int id);
+    CameraInfo getCameraInfo(int id);
+
+    CameraClass getCameraClassInfo(std::string name);
+    CameraClass getCameraClassInfo(int camId);
+private:
+    static Config* instance;
+
+    cv::FileStorage file;
+    std::unordered_map<int, CameraInfo> cameras;
+
+    Config(std::string filename);
+};
