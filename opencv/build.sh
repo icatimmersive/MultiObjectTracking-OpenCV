81ad8d92ea29323cd788dfe27050cbf97b16cee0
Anirudh Bagde <anidev.aelico@gmail.com>
2016-02-17 16:05:01 -0500

    Improved build script to control building contrib modules

--- opencv/build.sh
+++ opencv/build.sh
@@ -18,20 +18,23 @@ function echoE() {
 
 CONTRIBS="bgsegm"
 CMAKE_OPTIONS="-DCMAKE_BUILD_TYPE=Release -DWITH_QT=ON -DWITH_OPENGL=ON"
+CMAKE_CONTRIB_OPTIONS="-DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules"
 CMAKE_SAMPLES="-DBUILD_EXAMPLES=ON -DINSTALL_C_EXAMPLES=ON -DINSTALL_PYTHON_EXAMPLES=ON"
 PACKAGES="git cmake build-essential qt5-default python-dev python3-dev python-numpy python3-numpy default-jdk default-jre ant libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libavresample-dev libv4l-dev"
 
 INSTALL="n"
 SAMPLES="n"
+CONTRIB="n"
 
 while [ "x$#" != "x0" ]; do
 	case "x$1" in
 	"x--help")
 		echo1 "Build script for OpenCV 3.x"
 		echo ""
-		echo "Usage: $0 [--non-interactive [--install-packages] [--build-samples]]"
+		echo "Usage: $0 [--non-interactive [--install-packages] [--build-samples]] [--build-all-contrib]"
 		echo ""
 		echo "In non-interactive mode, by default required packages will not be installed and samples will not be built, unless otherwise specified."
+		echo "By default, only required extra opencv modules will be built. To build all extra modules, use --build-all-contrib."
 		exit
 		;;
 	"x--non-interactive")
@@ -43,6 +46,9 @@ while [ "x$#" != "x0" ]; do
 		;;
 	"x--build-samples")
 		SAMPLES="y"
+		;;
+	"x--build-all-contrib")
+		CONTRIB="y"
 		;;
 	esac
 	shift
@@ -104,19 +110,24 @@ else
 	git clone --depth=1 https://github.com/Itseez/opencv_contrib.git
 fi
 
-echo1 "Copying only required extra modules..."
-for mod in $CONTRIBS; do
-	echo2 " - $mod"
-	cp -r opencv_contrib/modules/$mod opencv/modules
-done
+if [ "x$CONTRIB" == "xy" ]; then
+	echo1 "\nBuilding all extra modules"
+	CMAKE_OPTIONS="$CMAKE_OPTIONS $CMAKE_CONTRIB_OPTIONS"
+else
+	echo1 "\nCopying only required extra modules..."
+	for mod in $CONTRIBS; do
+		echo2 " - $mod"
+		cp -r opencv_contrib/modules/$mod opencv/modules
+	done
+fi
 
-echo1 "Preparing OpenCV build..."
+echo1 "\nPreparing OpenCV build..."
 cd opencv
 mkdir build
 cd build
 cmake $CMAKE_OPTIONS ..
 
-echo1 "Press [ENTER] to begin building"
+echo1 "\nPress [ENTER] to begin building"
 if [ "x$NONINT" != "xy" ]; then
 	read
 fi
