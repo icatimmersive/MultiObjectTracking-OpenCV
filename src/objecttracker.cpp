ee61eb939ff060e7247f78437ac2470886eb9439
Anirudh Bagde <anidev.aelico@gmail.com>
2016-03-14 16:28:05 -0400

    Initial flann development progress

--- src/objecttracker.cpp
+++ src/objecttracker.cpp
@@ -1,6 +1,8 @@
 #include "objecttracker.h"
 #include <memory>
 #include <algorithm>
+#include <opencv2/flann/miniflann.hpp>
+#include "track.h"
 
 const int invisibleMax = 5;
 const double visibleThreshold = 0.6;
@@ -30,6 +32,7 @@ cv::UMat& ObjectTracker::getMaskImage() {
 void ObjectTracker::processContours(Tracks& tracks, std::vector<Contour>& contours) {
     // Delete lost tracks:
     // Tracks that are too sporadically visible, or have been invisible for too long
+    // TODO delete with heuristics
     for(auto it = tracks.begin(); it != tracks.end();) {
         std::unique_ptr<Track>& track = *it;
         if(isTrackLost(track)) {
@@ -39,6 +42,11 @@ void ObjectTracker::processContours(Tracks& tracks, std::vector<Contour>& contou
             it++;
         }
     }
+    // Using FLANN to group contours together by centroid
+   // std::vector<Contour> combinedContours;
+    //std::vector<cv::Point> contourPoints;
+    //std::transform(contours.begin(), contours.end(), contourPoints.begin(), calcCentroid);
+    
     // Assign contours to existing tracks
     Track::assignTracks(tracks, contours);
     // Create new tracks for unassigned contours
